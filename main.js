b65c9dd62d6be76277bc0c1fc241f0635f023f56
dhyancoding <142217143+dhyancoding@users.noreply.github.com>
2024-01-29 19:02:04 +1030

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-//https://teachablemachine.withgoogle.com/models/z5JNTfGMd/model.json
-
 prediction = " "
 
 Webcam.set({
@@ -22,4 +20,48 @@ classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models
 
 function model_loaded(){
     console.log("model.loaded")
+}
+
+
+function Predict(){
+    img1 = document.getElementById("snap1")
+    classifier.classify(img1, gotResults)
+}
+
+function gotResults(error, result){
+    if(error){
+        console.error(error)
+    }
+    else{
+        console.log(result)
+        document.getElementById("resultGesture").innerHTML = result[0].label
+        prediction = result[0].label
+        
+        console.log(result[0].label)
+
+        document.getElementById("resultConfidence").innerHTML = (result[0].confidence*100).toFixed(2) + "%"
+
+        Speak()
+
+        if(result[0].label=="Thumbs Up"){
+            document.getElementById("resultEmoji").innerHTML = "👍"
+        }
+
+        if(result[0].label=="Fine"){
+            document.getElementById("resultEmoji").innerHTML = "👌"
+        }
+
+
+        if(result[0].label=="Peace"){
+            document.getElementById("resultEmoji").innerHTML = "✌"
+        }
+    }
+}
+
+function Speak(){
+    s = window.speechSynthesis;
+    d1 = "I think the hand gesture you're doing is a " + prediction + " sign"
+    u = new SpeechSynthesisUtterance(d1)
+    u.rate = 0.5
+    s.speak(u)
 }
